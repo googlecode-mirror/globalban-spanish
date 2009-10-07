e493d747b1074c53e1fe5c0b1c9fbb626f41b638
apadrones <apadrones@af278e04-665f-11de-8d79-7368887f2767>
2009-10-07 16:29:46 +0000

    Add English and Spanish Languages to header.php

---
+++ globalban-spanish/banned/languages/Spanish/lan_header.php
@@ -0,0 +1,14 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     esGlobalBan - Language File.
+|
+|     $Source: /cvsroot/banned/languages/Spanish/lan_navigation.php,v $
+|     $Revision: 1.0 $
+|     $Date: 2009/07/02 30:36:39 $
+|     $Author: Odonel $
++----------------------------------------------------------------------------+
+*/
+
+$LANHEAD_001 = '  Gestor de Banes';
+?>
