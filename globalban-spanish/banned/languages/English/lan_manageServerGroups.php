942a210ae3ec222acdcfa8e76d4bffa3d87fbbc8
apadrones <apadrones@af278e04-665f-11de-8d79-7368887f2767>
2009-10-07 12:22:23 +0000

    Add English and Spanish Languages to manageServerGroups.php

---
+++ globalban-spanish/banned/languages/English/lan_manageServerGroups.php
@@ -0,0 +1,35 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     esGlobalBan - Language File.
+|
+|     $Source: /cvsroot/banned/languages/English/lan_manageServerGroups.php,v $
+|	  $Fichier : manageServerGroups.php
+|     $Revision: 1.0 $
+|     $Date: 2009/10/07 11:56:39 $
+|     $Author: Odonel $
++----------------------------------------------------------------------------+
+*/
+$LANDEMOS_001 = 'Do you really want to delete ';
+$LANDEMOS_002 = '?';
+$LANDEMOS_003 = 'Server Group List';
+$LANDEMOS_004 = 'Group Name';
+$LANDEMOS_005 = 'Description';
+$LANDEMOS_006 = 'Servers';
+$LANDEMOS_007 = 'Admins';
+$LANDEMOS_008 = 'Save';
+$LANDEMOS_009 = 'Delete';
+$LANDEMOS_010 = "Servers";
+$LANDEMOS_011 = 'No Servers';
+$LANDEMOS_012 = 'Click to manage the admins for ';
+$LANDEMOS_013 = 'Click to save settings for ';
+$LANDEMOS_014 = 'Click to delete ';
+$LANDEMOS_015 = 'Add New Group';
+$LANDEMOS_016 = 'Group Name:';
+$LANDEMOS_017 = 'Description:';
+$LANDEMOS_018 = 'Group Name Already Exists';
+$LANDEMOS_019 = 'Add Group';
+$LANDEMOS_020 = 'Note: If you add or delete a group then you should save your changes by clicking <b>&quot;Save Configuration&quot;</b> of the page ';
+$LANDEMOS_021 = 'Configuration';
+$LANDEMOS_022 = '.';
+?>
---
+++ globalban-spanish/banned/languages/Spanish/lan_manageServerGroups.php
@@ -0,0 +1,35 @@
+﻿<?php
+/*
++ ----------------------------------------------------------------------------+
+|     esGlobalBan - Language File.
+|
+|     $Source: /cvsroot/banned/languages/French/lan_manageServerGroups.php,v $
+|	  $Fichier : manageServerGroups.php
+|     $Revision: 1.0 $
+|     $Date: 2009/07/02 30:36:39 $
+|     $Author: Odonel $
++----------------------------------------------------------------------------+
+*/
+$LANDEMOS_001 = '¿Realmente desea borrar ';
+$LANDEMOS_002 = '?';
+$LANDEMOS_003 = 'Listado de grupos de Servidores';
+$LANDEMOS_004 = 'Nombre del grupo';
+$LANDEMOS_005 = 'Descripcion';
+$LANDEMOS_006 = 'Servidores';
+$LANDEMOS_007 = 'Administradores';
+$LANDEMOS_008 = 'Guardar Cambios';
+$LANDEMOS_009 = 'Borrar';
+$LANDEMOS_010 = 'Servidores';
+$LANDEMOS_011 = 'No hay servidores';
+$LANDEMOS_012 = 'Haga Click para administrar los admins para ';
+$LANDEMOS_013 = 'Haga Click para guardar los cambios para ';
+$LANDEMOS_014 = 'haga Click para borrar ';
+$LANDEMOS_015 = 'A&ntilde;adir un Nuevo Grupo';
+$LANDEMOS_016 = 'Nombre del grupo:';
+$LANDEMOS_017 = 'Descripcion:';
+$LANDEMOS_018 = 'Ese nombre de grupo ya existe';
+$LANDEMOS_019 = 'A&ntilde;adir Grupo';
+$LANDEMOS_020 = 'Nota: Si añade o elimina un grupo, entonces usted debe guardar los cambios haciendo clic en <b>&quot;Guardar la configuración&quot;</b> de la pagina ';
+$LANDEMOS_021 = 'Configuracion';
+$LANDEMOS_022 = '.';
+?>
