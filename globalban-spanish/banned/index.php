cc2ad88fb57c2e0cf35cd746e6a41b331f50b0f2
apadrones <apadrones@af278e04-665f-11de-8d79-7368887f2767>
2010-05-27 09:16:00 +0000

    Removed UTF-8 "Persist BOM" in German lan_ files

--- globalban-spanish/banned/index.php
+++ globalban-spanish/banned/index.php
@@ -16,7 +16,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with GlobalBan.  If not, see <http://www.gnu.org/licenses/>.
+    along with GlobalBan.  If not, see <http://www.gnu.org/licenses/>.ñ
 */
 
 define("ROOTDIR", dirname(__FILE__)); // Global Constant of root directory
@@ -30,7 +30,7 @@ require_once(ROOTDIR."/include/php4functions.php");
 // ############################### INIT #######################################
 $db = new Database(); // Just about every page makes a database call
 $config = new Config(); // All configuration variables are contained in this object
-$version = "v3.4.1 r128";
+$version = "v3.4.1 r129";
 $copyright = "Developed by <a href='mailto:lsjonasson@gmail.com'>Soynuts</a>, <a href='mailto:apadrones@gmail.com'>Odonel</a> and <a href='mailto:fantole@gmail.com'>Fantole</a> &copy;2007-2010";
 $translators = "Translators: French by <a href='mailto:alpha.cssupport@gmail.com'>Owned</a>, Russian by <a href='http://universal-games.ru/'>Co0l</a>, Spanish by <a href='mailto:apadrones@gmail.com'>Odonel</a> and German by <a href='http://www.clan-holyshit.de/'>Neo@Bln + RST_Freak</a>";
 // ############################################################################
